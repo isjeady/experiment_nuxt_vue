7aff6b49f057d3efa5fb38b8f9010b10912172a2
Leandro <vitto.leandro@gmail.com>
2019-11-28 23:20:18 +0100

    DisplayResults

--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,51 @@
                   Search
                 </h1>
             </div>
+            <ais-search-box placeholder="Scrivi cosa cercare..." />
         </div>
+
+        <ais-state-results>
+          <div slot-scope="{ query, hits }">
+            <!-- {{ hits.length }} -->
+            
+            <div v-if="hits.length">
+              <ais-stats>
+                  <h1 class="text-md mb-10 text-gray-700" slot-scope="{ hitsPerPage, nbPages, nbHits, page, processingTimeMS, query }">
+                    Page {{ page + 1 }} of {{ nbPages }} with {{ hitsPerPage }} hits per page  -
+                    {{ nbHits }} hits retrieved in {{ processingTimeMS }}ms for <q>{{ query }}</q>
+                  </h1>
+              </ais-stats>
+              <ais-hits>
+                <div slot="item" slot-scope="{ item, index }">
+                  {{ index }} - {{ item }}
+                </div>
+              </ais-hits>
+            </div>
+
+            <p v-if="!hits.length">
+              No results found for the query: <q>{{ query }}</q>
+            </p>
+
+          </div>
+        </ais-state-results>
+
       </ais-instant-search-ssr> 
     </div>
 </template>
 
 <script>
 	import {
-      AisInstantSearchSsr
+      AisInstantSearchSsr,AisHits,AisSearchBox,AisStateResults,AisStats
   } from 'vue-instantsearch';
   
   
   export default {
     components : {
-      AisInstantSearchSsr
+      AisInstantSearchSsr,
+      AisSearchBox,
+      AisStateResults,
+      AisStats,
+      AisHits
     },
 		mounted(){
       console.log('Mounted');
@@ -44,7 +75,7 @@
         const instantsearch = app.$instantsearch;
         
         return instantsearch.findResultsState({
-          query : 'samsung',
+          //query : 'samsung',
           hitsPerPage : 10
         }).then(() => ({
           algoliaState: instantsearch.getState()
